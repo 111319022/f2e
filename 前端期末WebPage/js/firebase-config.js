861cf5c3af1eca2d5a9102db48ea1b56b8f3db5e
111319022 <s111319022@stu.ntue.edu.tw>
2025-12-14 21:16:48 +0800

    Update firebase-config.js

--- 前端期末WebPage/js/firebase-config.js
+++ 前端期末WebPage/js/firebase-config.js
@@ -2,8 +2,9 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-storage.js";
 
-// 這裡貼上你真實的 Config
+//  Config
 const firebaseConfig = {
   apiKey: "AIzaSyA6odCZUzRQVwlMv4Pnu55qGKwEFv2fzeo",
   authDomain: "ntue-moodle-resedign.firebaseapp.com",
@@ -14,9 +15,10 @@ const firebaseConfig = {
   measurementId: "G-G0M5D0JNZG"
 };
 
-// 初始化一次就好
+// 初始化一次
 const app = initializeApp(firebaseConfig);
 
 // 把準備好的 auth 和 db 匯出給別人用
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+export const storage = getStorage(app);
